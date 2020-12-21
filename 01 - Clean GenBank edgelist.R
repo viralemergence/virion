c6a1099df1c5bac3d9e4f11ca09ba520f4ba76fd
Colin J. Carlson <cjcarlson@berkeley.edu>
2020-12-20 21:36:10 -0500

    The taxonomy two-step begins: step one

--- 01 - Clean GenBank edgelist.R
+++ 01 - Clean GenBank edgelist.R
@@ -2,17 +2,17 @@
 library(tidyverse)
 setwd("~/Github/virion")
 
-source("00-taxize-cleaner-clone.R")
+source("00 - taxize-cleaner-clone.R")
 
-# read in host data
+# Read in host data
 
+unzip("GenBank_as_Edgelist.zip", exdir = getwd())
 gb <- read_csv("GenBank_as_Edgelist.csv")
-
 hosts_vec <- unique(na.omit(gb$Host))
+synonyms <- lapply(1:20, findSyns3) # length(hosts_vec)
+# ^ This is the one you're gonna want to switcheroo
+synonyms %>% bind_rows() -> host.dictionary
 
-start = Sys.time()
-synonyms_lookup = lapply(1:500, findSyns3)
-end = Sys.time()
-end-start
-
-synonyms_lookup %>% bind_rows() -> host.dictionary
+gb2 <- left_join(gb, host.dictionary, by = c('Host' = 'Original'))
+write_csv(gb2, 'GenBank-Taxized.csv')
+zip(zipfile = 'GBTaxized.zip', files = 'GenBank-Taxized.csv')
