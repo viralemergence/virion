bef413de453c0c0d50f21f0509e7b684e44e1ea0
Colin J. Carlson <cjcarlson@berkeley.edu>
2020-12-20 21:42:49 -0500

    teeny tiny change

--- 01 - Clean GenBank edgelist.R
+++ 01 - Clean GenBank edgelist.R
@@ -9,7 +9,7 @@ source("00 - taxize-cleaner-clone.R")
 unzip("GenBank_as_Edgelist.zip", exdir = getwd())
 gb <- read_csv("GenBank_as_Edgelist.csv")
 hosts_vec <- unique(na.omit(gb$Host))
-synonyms <- lapply(1:20, findSyns3) # length(hosts_vec)
+synonyms <- lapply(1:length(hosts_vec), findSyns3) # 1:20 
 # ^ This is the one you're gonna want to switcheroo
 synonyms %>% bind_rows() -> host.dictionary
 
