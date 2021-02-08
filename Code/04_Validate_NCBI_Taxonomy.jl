eb3f38709941621338726ce74ef8be7499ed9aae
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-08 12:00:43 -0500

    basic code working

--- Code/04_Validate_NCBI_Taxonomy.jl
+++ Code/04_Validate_NCBI_Taxonomy.jl
@@ -1,13 +1,14 @@
 using NCBITaxonomy
 using DataFrames
 import CSV
+using ProgressMeter
 
 # Read the virion master data
 virion = DataFrame(CSV.File(joinpath("Virion", "Virion-Master.csv")))
 
 # DF for results
-virion_cleanup = DataFrame(original = String[], name = String[], type = String[], taxid = Int64[], fuzzy = Bool[], synonym = Bool[])
-virion_tax = DataFrame(taxid = String[], rank = String[], name = String[], id = Int64[])
+virion_cleanup = DataFrame(original = String[], name = String[], type = String[], taxid = Int64[], fuzzy = Bool[], different = Bool[])
+virion_tax = DataFrame(taxid = Int64[], rank = Symbol[], name = String[], id = Int64[])
 
 unique_hosts = unique(virion.Host)
 unique_viruses = unique(virion.Virus)
@@ -15,14 +16,51 @@ unique_viruses = unique(virion.Virus)
 hf = vertebratefinder(true)
 vf = virusfinder()
 
-for host in unique_hosts[1:10]
+@showprogress for host in unique_hosts
     ncbi_tax = hf(host)
     fuzzy = false
     if isnothing(ncbi_tax)
         fuzzy = true
         ncbi_tax = hf(host; fuzzy=true)
     end
+    # Get the cleaned name
     push!(virion_cleanup,(
-        host, ncbi_tax.name, "host", ncbi_tax.id, fuzzy, host == ncbi_tax.name
+        host, ncbi_tax.name, "host", ncbi_tax.id, fuzzy, host != ncbi_tax.name
     ))
+    # Get the taxonomy
+    lin = lineage(ncbi_tax)
+    rnk = rank.(lin)
+    class = findfirst(isequal(:class), rnk)
+    for i in class:length(rnk)
+        push!(virion_tax,(
+            ncbi_tax.id, rnk[i], lin[i].name, lin[i].id
+        ))
+    end
 end
+
+
+@showprogress for virus in unique_viruses
+    ncbi_tax = vf(virus)
+    fuzzy = false
+    if isnothing(ncbi_tax)
+        fuzzy = true
+        ncbi_tax = vf(virus; fuzzy=true)
+    end
+    # Get the cleaned name
+    push!(virion_cleanup,(
+        virus, ncbi_tax.name, "virus", ncbi_tax.id, fuzzy, virus != ncbi_tax.name
+    ))
+    # Get the taxonomy
+    lin = lineage(ncbi_tax)
+    rnk = rank.(lin)
+    class = findfirst(isequal(:class), rnk)
+    class = isnothing(class) ? 1 : class
+    for i in class:length(rnk)
+        push!(virion_tax,(
+            ncbi_tax.id, rnk[i], lin[i].name, lin[i].id
+        ))
+    end
+end
+
+CSV.write("cleaned_named.csv", virion_cleanup)
+CSV.write("taxonomy.csv", virion_tax)
