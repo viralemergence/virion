5b3fb97e719e637d5f11453f0e5462d62a7fb892
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-01-05 20:35:24 -0500

    path

--- Code/Code_Dev/virus.jl
+++ Code/Code_Dev/virus.jl
@@ -2,7 +2,7 @@ using NCBITaxonomy
 using DataFrames
 import CSV
 
-include(joinpath(@__DIR__, "Code/Code_Dev/taxonomizer.jl"))
+include(joinpath(@__DIR__, "/Code/Code_Dev/taxonomizer.jl"))
 
 names = DataFrame(CSV.File(joinpath(@__DIR__, "/Code/Code_Dev/TaxonomyTempIn.csv"); delim=';'))
 reconciled_names = taxonomizer(names, :pathogens; names=:Name)
