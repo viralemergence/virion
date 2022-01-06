733752ca9ff88d407fdaa98ad7612c6739b05a67
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-01-05 20:34:59 -0500

    fix path

--- Code/Code_Dev/virus.jl
+++ Code/Code_Dev/virus.jl
@@ -6,4 +6,4 @@ include(joinpath(@__DIR__, "Code/Code_Dev/taxonomizer.jl"))
 
 names = DataFrame(CSV.File(joinpath(@__DIR__, "/Code/Code_Dev/TaxonomyTempIn.csv"); delim=';'))
 reconciled_names = taxonomizer(names, :pathogens; names=:Name)
-CSV.write(joinpath(@__DIR__, "TaxonomyTempOut.csv"), leftjoin(names, reconciled_names, on=:Name => :Original))
+CSV.write(joinpath(@__DIR__, "/Code/Code_Dev/TaxonomyTempOut.csv"), leftjoin(names, reconciled_names, on=:Name => :Original))
