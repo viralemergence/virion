f83617cddb5d98ebab7030cf4696bd7f1bf59e88
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-01-06 15:46:37 -0500

    CLOVER case

--- Code/03_Merge clean files.R
+++ Code/03_Merge clean files.R
@@ -4,7 +4,7 @@ library(tidyverse); library(magrittr); library(vroom)
 source("./Code/001_TaxizeFunctions.R")
 
 gb <- vroom("./Intermediate/Formatted/GenbankFormatted.csv.gz", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
-clo <- read_csv("./Intermediate/Formatted/CloverFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+clo <- read_csv("./Intermediate/Formatted/CLOVERFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
 pred <- read_csv("./Intermediate/Formatted/PREDICTAllFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
 globi <- read_csv("./Intermediate/Formatted/GLOBIFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
 
