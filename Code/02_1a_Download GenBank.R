7fab8b3f7f0758de45251a9b1da9b6728ffb68fb
Colin J. Carlson <cjcarlson@berkeley.edu>
2022-01-06 12:09:58 -0500

    Fixing the .csv.gz pointer for the new GenBank
    Including installing R.utils, which allows data.table::fread() to directly read a .csv.gz

--- Code/02_1a_Download GenBank.R
+++ Code/02_1a_Download GenBank.R
@@ -1,19 +1,20 @@
 
 library(RCurl)
 library(readr)
+library(R.utils)
 library(tidyverse)
 library(vroom)
 
-url = "https://ftp.ncbi.nlm.nih.gov/genomes/Viruses/AllNuclMetadata/AllNuclMetadata.csv"
-d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv"), error = function(e){-999})
+url = "https://ftp.ncbi.nlm.nih.gov/genomes/Viruses/AllNuclMetadata/AllNuclMetadata.csv.gz"
+d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv.gz"), error = function(e){-999})
 if(d == -999) {
   while (d == -999){
     Sys.sleep(600)
-    d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv"), error = function(e){-999})
+    d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv.gz"), error = function(e){-999})
   }
 }
 
-seq <- data.table::fread("./Source/AllNuclMetadata.csv",
+seq <- data.table::fread("./Source/AllNuclMetadata.csv.gz",
                          select = c("#Accession", "Release_Date", "Species", "Host", "Collection_Date"))
 seq %>% rename(Accession = "#Accession") %>% 
   vroom_write("./Source/sequences.csv")
