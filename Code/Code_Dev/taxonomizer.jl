59cddd253c6fe8d3c53308faff50f6267a93e6c7
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-01-06 10:28:45 -0500

    julia script fixed

--- Code/Code_Dev/taxonomizer.jl
+++ Code/Code_Dev/taxonomizer.jl
@@ -16,66 +16,67 @@ changed in Julia configuration file, on the command line when calling julia
 """
 function taxonomizer(df::DataFrame, type::Symbol=:hosts; names::Symbol=:Name)
 
-# Check that the name type is either hosts or pathogens
-@assert type ∈ [:hosts, :pathogens]
+    # Check that the name type is either hosts or pathogens
+    @assert type ∈ [:hosts, :pathogens]
 
-# And build the appropriate nametype
-namelist = isequal(:hosts)(type) ? vertebratefilter() : virusfilter()
+    # And build the appropriate nametype
+    namelist = isequal(:hosts)(type) ? vertebratefilter() : virusfilter()
 
-# Remove blast names
-filter!(row -> row.class != NCBITaxonomy.class_blast_name, namelist)
+    # Remove blast names
+    filter!(row -> row.class != NCBITaxonomy.class_blast_name, namelist)
 
-# Get the names from the correct column (usually `Name` but we can change it with the `names` argument)
-synonyms = unique(uppercasefirst.(df[:,names]))
+    # Get the names from the correct column (usually `Name` but we can change it with the `names` argument)
+    synonyms = unique(uppercasefirst.(df[:, names]))
 
-# Prepare a dataframe from every thread
-results = DataFrame(
-    Original = String[],
-    Rank = Symbol[],
-    TaxId = Union{Missing,Int64}[],
-    Species = Union{Missing,String}[],
-    Genus = Union{Missing,String}[],
-    Order = Union{Missing,String}[],
-    Class = Union{Missing,String}[],
-    Family = Union{Missing,String}[]
-)
+    # Prepare a dataframe from every thread
+    results = DataFrame(;
+        Original=String[],
+        Rank=Symbol[],
+        TaxId=Union{Missing,Int64}[],
+        Species=Union{Missing,String}[],
+        Genus=Union{Missing,String}[],
+        Order=Union{Missing,String}[],
+        Class=Union{Missing,String}[],
+        Family=Union{Missing,String}[],
+    )
 
-# Prepare the progressbar
-n = length(synonyms)
+    # Prepare the progressbar
+    n = length(synonyms)
 
-# GO BR
-for i in 1:n
-    try
-        nm = taxon(namelist, synonyms[i]; casesensitive=false)
-        df_row = _prepare_name_tuple(synonyms[i], nm)
-        push!(results, df_row)
-    catch err
-        if isa(err, NameHasNoDirectMatch)
+    # GO BR
+    for i in 1:n
+        @info synonyms[i]
+        try
+            nm = taxon(namelist, synonyms[i]; casesensitive=false, preferscientific=true)
+            df_row = _prepare_name_tuple(synonyms[i], nm)
+            push!(results, df_row)
+        catch err
+            if isa(err, NameHasNoDirectMatch)
+                continue
+            end
+            if isa(err, NameHasMultipleMatches)
+                for nm in err.taxa
+                    df_row = _prepare_name_tuple(synonyms[i], nm)
+                    push!(results, df_row)
+                end
+            end
             continue
         end
-        if isa(err, NameHasMultipleMatches)
-            for nm in alternativetaxa(namelist, synonyms[i])
-                df_row = _prepare_name_tuple(synonyms[i], nm)
-                push!(results, df_row)
-            end
-        end
-        continue
     end
-end
-return results
+    return results
 end
 
 function _prepare_name_tuple(n, taxa; levels=[:species, :genus, :order, :class, :family])
-ln = lineage(taxa)
-rk = rank.(ln)
-match = Any[n, rank(taxa), taxa.id]
-for level in levels
-    rnk_i = findfirst(isequal(level), rk)
-    if isnothing(rnk_i)
-        push!(match, missing)
-    else
-        push!(match, ln[rnk_i].name)
+    ln = lineage(taxa)
+    rk = rank.(ln)
+    match = Any[n, rank(taxa), taxa.id]
+    for level in levels
+        rnk_i = findfirst(isequal(level), rk)
+        if isnothing(rnk_i)
+            push!(match, missing)
+        else
+            push!(match, ln[rnk_i].name)
+        end
     end
-end
-return tuple(match...)
+    return tuple(match...)
 end
