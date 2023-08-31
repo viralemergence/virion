7021afd7f29194905b3151ebbbc8d15883683a74
Cole Brookson <cole.brookson@gmail.com>
2023-08-31 01:58:01 -0400

    another package attempt

--- Code/04_High level VIRION checks.R
+++ Code/04_High level VIRION checks.R
@@ -2,11 +2,13 @@
 library(tidyverse); library(vroom); library(magrittr)
 if(!exists('vdict')) {source('Code/001_TaxizeFunctions.R')}
 
-virion <- vroom("./Intermediate/Formatted/VIRIONUnprocessed.csv.gz", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+virion <- vroom::vroom("./Intermediate/Formatted/VIRIONUnprocessed.csv.gz", 
+                col_type = cols(PMID = col_double(), 
+                                PublicationYear = col_double()))
 
 # # Is there anything that's not vertebrate in here?
 # 
-# virion %>% filter(!(HostClass %in% c("actinopteri",
+# virion %>% dplyr::filter(!(HostClass %in% c("actinopteri",
 #                                      "actinopterygii",
 #                                      "amphibia",
 #                                      "aves",
@@ -17,14 +19,14 @@ virion <- vroom("./Intermediate/Formatted/VIRIONUnprocessed.csv.gz", col_type = 
 #                                      "mammalia",
 #                                      "myxini",
 #                                      "reptilia"))) %>%
-#   filter(!is.na(HostClass)) %>% View()
+#   dplyr::filter(!is.na(HostClass)) %>% View()
 
 # Deal with the phage 
   
 virion %<>% 
-  filter(!str_detect(Virus, "phage")|is.na(Virus),
-         !str_detect(Virus, "bacteri")|is.na(Virus)) %>%
-  filter(!(VirusFamily %in% c("turriviridae",
+  dplyr::filter(!stringr::str_detect(Virus, "phage")|is.na(Virus),
+         !stringr::str_detect(Virus, "bacteri")|is.na(Virus)) %>%
+  dplyr::filter(!(VirusFamily %in% c("turriviridae",
                              "ackermannviridae",
                              "myoviridae",
                              "siphoviridae",
@@ -63,24 +65,27 @@ virion %<>%
                              "tubulavirales",
                              "vinavirales")))
 
-ictv <- read_csv("Source/ICTV Master Species List 2019.v1.csv")
+ictv <- readr::read_csv("Source/ICTV Master Species List 2019.v1.csv")
 
-virion %<>% mutate(ICTVRatified = (Virus %in% str_to_lower(ictv$Species))) %>%
-  relocate(ICTVRatified, .after = VirusNCBIResolved)
+virion %<>% dplyr::mutate(
+  ICTVRatified = (Virus %in% stringr::str_to_lower(ictv$Species))) %>%
+  dplyr::relocate(ICTVRatified, .after = VirusNCBIResolved)
 
-virion %<>% mutate(HostFlagID = replace_na(HostFlagID, FALSE)) # This only applies to CLOVER and GLOBI, which both don't have any other internal flags
+# This only applies to CLOVER and GLOBI, which both don't have any other internal flags
+virion %<>% dplyr::mutate(HostFlagID = replace_na(HostFlagID, FALSE)) 
 
-virion %<>% mutate_cond(str_detect(HostOriginal, " cf\\."), HostFlagID = TRUE) 
+virion %<>% mutate_cond(stringr::str_detect(HostOriginal, " cf\\."), HostFlagID = TRUE) 
 
-virion %<>% select(-c(HostSynonyms))
+virion %<>% dplyr::select(-c(HostSynonyms))
 
 ####
 
 virion %<>% distinct()
-virion %<>% mutate_all(as.character) %>% mutate_all(~replace_na(.x, ''))
+virion %<>% dplyr::mutate_all(as.character) %>% 
+  dplyr::mutate_all(~tidyr::replace_na(.x, ''))
 
 virion %<>% 
-  group_by_at(vars(-NCBIAccession)) %>% 
-  summarize(NCBIAccession = str_c(NCBIAccession, collapse = ", "))
+  dplyr::group_by_at(dplyr::vars(-NCBIAccession)) %>% 
+  dplyr::summarize(NCBIAccession = stringr::str_c(NCBIAccession, collapse = ", "))
 
-vroom_write(virion, "Virion/Virion.csv.gz")
+vroom::vroom_write(virion, "Virion/Virion.csv.gz")
