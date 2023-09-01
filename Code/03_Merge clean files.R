989026c2c739dd0124f0e41d202e04c61b6b5e53
Cole Brookson <cole.brookson@gmail.com>
2023-09-01 01:55:44 -0400

    trying encoding again

--- Code/03_Merge clean files.R
+++ Code/03_Merge clean files.R
@@ -1,18 +1,23 @@
 
-library(tidyverse); library(magrittr); library(vroom)
+library(tidyverse); library(magrittr); library(vroom); library(tidyft)
 
 source("./Code/001_TaxizeFunctions.R")
 
-gb <- vroom("./Intermediate/Formatted/GenbankFormatted.csv.gz", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
-clo <- read_csv("./Intermediate/Formatted/CLOVERFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
-pred <- read_csv("./Intermediate/Formatted/PREDICTAllFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
-globi <- read_csv("./Intermediate/Formatted/GLOBIFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+vroom::locale(encoding = "UTF-8")
 
-if(class(clo$NCBIAccession)=='numeric') {clo %<>% mutate(NCBIAccession = as.character(NCBIAccession))}
+gb <- vroom("./Intermediate/Formatted/GenbankFormatted.csv.gz", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+clo <- vroom("./Intermediate/Formatted/CLOVERFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+pred <- vroom("./Intermediate/Formatted/PREDICTAllFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+globi <- vroom("./Intermediate/Formatted/GLOBIFormatted.csv", col_type = cols(PMID = col_double(), PublicationYear = col_double()))
+
+if(is.numeric(clo$NCBIAccession)) {clo %<>% dplyr::mutate(NCBIAccession = as.character(NCBIAccession))}
 
 virion <- dplyr::bind_rows(clo, pred, gb, globi)
-
-vroom::locale(encoding = "UTF-8")
+  
+# chr_cols <- names(virion[, sapply(virion, is.character)])
+#   
+# virion <- virion %>%
+#   tidyft::utf8_encoding(chr_cols) 
 
 vroom::vroom_write(virion, "./Intermediate/Formatted/VIRIONUnprocessed.csv.gz")
 
