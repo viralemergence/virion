714cddfd6a6b224daa4258e0e519d099f869674a
colebrookson <cole.brookson@gmail.com>
2023-09-19 14:41:04 -0400

    template file

---
+++ Code/00_File_Template.R
@@ -0,0 +1,13 @@
+# Set-up =======================================================================
+
+#' have all the source calls and library calls in the top of the file 
+
+# Section 1 ====================================================================
+
+#' In each section, all code should be in as few chunks as possible and as much 
+#' in base code as possible (fewer dependency issues)
+
+# Last Section =================================================================
+
+#' Ensure that any writing out of objects all happens at the end and that we 
+#' do any small tests here
