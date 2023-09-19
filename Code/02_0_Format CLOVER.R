2d93ed1fb8410e8f8b639bd65de1ebbebbb153df
colebrookson <cole.brookson@gmail.com>
2023-09-19 17:08:28 -0400

    docs with GenBank

--- Code/02_0_Format CLOVER.R
+++ Code/02_0_Format CLOVER.R
@@ -1,9 +1,18 @@
+#' Format CLOVER 
+#' 
+#' Clover is a static file that comes from the clover database and you need to 
+#' read it in from that location
 
+# Set up =======================================================================
 library(tidyverse)
 library(magrittr)
 library(lubridate)
 
-clo <- read_csv("~/Github/clover/clover/clover_1.0_allpathogens/CLOVER_1.0_Viruses_AssociationsFlatFile.csv")
+clo <- readr::read_csv(here::here(paste0(
+  "../clover/clover/clover_1.0_allpathogens/",
+  "CLOVER_1.0_Viruses_AssociationsFlatFile.csv")))
+
+# template dataframe ===========================================================
 
 temp <- data.frame(Host = character(),
                  Virus = character(),
@@ -39,18 +48,23 @@ temp <- data.frame(Host = character(),
                  CollectionDay = double(),
                  stringsAsFactors = FALSE)
 
+# do some cleaning on clover ===================================================
+
 # This step should go away after Rory updates CLOVERT with the "sp." thing
-if(!("HostGenus" %in% colnames(clo))) { clo %<>% mutate(HostGenus = word(Host, 1)) }
+if(!("HostGenus" %in% colnames(clo))) { 
+  clo %<>% mutate(HostGenus = word(Host, 1)) }
 
 # This step should go away after Rory updates to remove CitationID
 if("CitationID" %in% colnames(clo)) { 
     clo %>% 
     rowwise() %>%
-    mutate(NCBIAccession = ifelse(CitationIDType=='NCBI Nucleotide', CitationID, NA),
+    mutate(NCBIAccession = 
+             ifelse(CitationIDType=='NCBI Nucleotide', CitationID, NA),
            PMID = ifelse(CitationIDType=='PMID', CitationID, NA)) %>%
     select(-c(CitationID, CitationIDType)) -> clo
 }
 
+# names need to be consistent
 clo %<>% rename(Virus = "Pathogen",
                VirusGenus = "PathogenGenus",
                VirusFamily = "PathogenFamily",
@@ -68,17 +82,16 @@ clo %<>% select(-c(PathogenType,
                    Detection_Isolation))
 
 clo %<>% mutate(NCBIAccession = as.character(NCBIAccession))
-
 clo %<>% select(-ICTVRatified)
-
-# colnames(clo)[!(colnames(clo) %in% colnames(temp))] # Deleted check 
-
-bind_rows(temp, clo) -> clo
+dplyr::bind_rows(temp, clo) -> clo
 
 # Consistency steps: all lowercase names
-
-clo %<>% mutate_at(c("Host", "HostGenus", "HostFamily", "HostOrder", "HostClass",
-                     "Virus", "VirusGenus", "VirusFamily", "VirusOrder", "VirusClass"),
+clo %<>% dplyr::mutate_at(c("Host", "HostGenus", "HostFamily", "HostOrder", 
+                     "HostClass","Virus", "VirusGenus", "VirusFamily", 
+                     "VirusOrder", "VirusClass"),
                    tolower)
 
-write_csv(clo, 'Intermediate/Formatted/CLOVERFormatted.csv')
+# write out ====================================================================
+
+readr::write_csv(clo, 
+                 here::here("./Intermediate/Formatted/CLOVERFormatted.csv"))
--- Code/02_1a_Download GenBank.R
+++ Code/02_1a_Download GenBank.R
@@ -1,3 +1,9 @@
+#' Download GenBank 
+#' 
+#' GenBank is an updating resource so you have to pull it and this script does 
+#' that 
+
+# set up =======================================================================
 
 library(RCurl)
 library(readr)
@@ -5,16 +11,28 @@ library(R.utils)
 library(tidyverse)
 library(vroom)
 
-url = "https://ftp.ncbi.nlm.nih.gov/genomes/Viruses/AllNuclMetadata/AllNuclMetadata.csv.gz"
-d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv.gz"), error = function(e){-999})
+# download and process =========================================================
+
+url = paste0("https://ftp.ncbi.nlm.nih.gov/genomes/Viruses/AllNuclMetadata/",
+             "AllNuclMetadata.csv.gz")
+d = tryCatch(download.file(url, destfile = here::here(
+  "./Source/AllNuclMetadata.csv.gz")),
+  error = function(e){-999})
+
 if(d == -999) {
   while (d == -999){
     Sys.sleep(600)
-    d = tryCatch(download.file(url, destfile = "./Source/AllNuclMetadata.csv.gz"), error = function(e){-999})
+    d = tryCatch(download.file(
+      url, destfile = here::here("./Source/AllNuclMetadata.csv.gz")), 
+      error = function(e){-999})
   }
 }
 
-seq <- data.table::fread("./Source/AllNuclMetadata.csv.gz",
-                         select = c("#Accession", "Release_Date", "Species", "Host", "Collection_Date"))
-seq %>% rename(Accession = "#Accession") %>% 
-  vroom_write("./Source/sequences.csv")
+# reading this in - use data.table
+seq <- data.table::fread(here::here("./Source/AllNuclMetadata.csv.gz"),
+                         select = c("#Accession", "Release_Date", "Species", 
+                                    "Host", "Collection_Date"))
+seq %>% rename(Accession = "#Accession")
+
+# write out ==================================================================== 
+vroom::vroom_write(seq, here::here("./Source/sequences.csv"))
