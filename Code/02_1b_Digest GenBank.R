31cb8e822bc2db03d724935f748852fa74089652
colebrookson <cole.brookson@gmail.com>
2023-09-19 17:18:44 -0400

    documentation documentation documentation

--- Code/02_1b_Digest GenBank.R
+++ Code/02_1b_Digest GenBank.R
@@ -1,21 +1,34 @@
+#' Genback processing
+#' 
+#' There's some cleaning to do, and that's done here
+#' 
 
-# 01_Setup ####
-
+# set up =======================================================================
 rm(list = ls())
 
-library(tidyverse); library(taxize); library(magrittr); library(fs); library(zip); library(vroom)
+library(dplyr)
+library(taxize)
+library(magrittr)
+library(fs)
+library(zip)
+library(vroom)
+library(rentrez)
+
+# this is necessary for using the NCBI records of more than 10
 rentrez::set_entrez_key("ec345b39079e565bdfa744c3ef0d4b03ba08")
 
-if(!exists('vdict')) {source('Code/001_TaxizeFunctions.R')}
-if(!exists('jvdict')) {source('Code/001_Julia functions.R')}
+# get the functions to do all the dictionary stuff
+if(!exists('vdict')) {source(here::here("./Code/001_TaxizeFunctions.R"))}
+if(!exists('jvdict')) {source(here::here("./Code/001_Julia functions.R"))}
 
-if(!file.exists("Source/sequences.csv")){
-  unzip("Source/GenBank.zip", exdir = 'Source')
+if(!file.exists(here::here("./Source/sequences.csv"))){
+  unzip(here::here("./Source/GenBank.zip"), exdir = 'Source')
 }
 
-gb <- data.table::fread("Source/sequences.csv") %>% 
-  as_tibble
+gb <- data.table::fread(here("./Source/sequences.csv")) %>% 
+  dplyr::as_tibble()
 
+# do the cleaning itself =======================================================
 gb %>% pull(Host) %>% unique() %>% sort() -> host.list
 host.table <- jhdict(host.list)
 write_csv(host.table, 'Intermediate/GBHostTax.csv')
@@ -32,14 +45,19 @@ gb %<>% rename(HostOriginal = "Host") %>%
                           "Lepidosauria",
                           "Mammalia",
                           "Myxini",
-                          "Reptilia") | HostOrder %in% c("Testudines", "Crocodylia"))
- # Reptilia is defunct but left in case GLOBI has something on it or it's reinstituted or something weird
+                          "Reptilia") | HostOrder %in% 
+           c("Testudines", "Crocodylia"))
+            # Reptilia is defunct but left in case GLOBI has something on it 
+            # or it's reinstituted or something weird
 
 gb %>% pull(Species) %>% unique() %>% sort() -> virus.list
 virus.table <- jvdict(virus.list)
-write_csv(virus.table, 'Intermediate/GBVirusTax.csv')
 
 gb %<>% rename(VirusOriginal = "Species") %>%
   left_join(virus.table)
 
-vroom_write(gb, "Intermediate/Unformatted/GenBankUnformatted.csv.gz")
+# write files ==================================================================
+vroom::vroom_write(gb, here::here(
+  "./Intermediate/Unformatted/GenBankUnformatted.csv.gz"))
+readr::write_csv(virus.table, here::here("./Intermediate/GBVirusTax.csv"))
+
