d0d48a78671cc280d9440f3c212c9c45a7d1ae6c
Cole Brookson <cole.brookson@gmail.com>
2023-11-25 14:36:38 -0500

    Revert "docker image working"

--- Code/02_1a_Download GenBank.R
+++ Code/02_1a_Download GenBank.R
@@ -11,17 +11,24 @@ library(magrittr)
 
 url = paste0("https://ftp.ncbi.nlm.nih.gov/genomes/Viruses/AllNuclMetadata/",
              "AllNuclMetadata.csv.gz")
+d = tryCatch(utils::download.file(url, destfile = here::here(
+  "./Source/AllNuclMetadata.csv.gz")),
+  error = function(e){-999})
 
-location <- here::here("./Source/") 
-system(paste0("wget ", url, " -P ", location))
+if(d == -999) {
+  while (d == -999){
+    Sys.sleep(600)
+    d = tryCatch(utils::download.file(
+      url, destfile = here::here("./Source/AllNuclMetadata.csv.gz")), 
+      error = function(e){-999})
+  }
+}
 
 # reading this in - use data.table
 seq <- data.table::fread(here::here("./Source/AllNuclMetadata.csv.gz"),
                          select = c("#Accession", "Release_Date", "Species", 
                                     "Host", "Collection_Date"))
-print("readin")
-seq %<>% dplyr::rename(Accession = "#Accession")  
+seq %<>% dplyr::rename(Accession = "#Accession")
 
 # write out ==================================================================== 
 vroom::vroom_write(seq, here::here("./Source/sequences.csv"))
-print("written")
--- Code/Code_Dev/host.jl
+++ Code/Code_Dev/host.jl
@@ -1,5 +1,5 @@
-using DataFrames
 using NCBITaxonomy
+using DataFrames
 import CSV
 
 include(joinpath(pwd(), "Code/Code_Dev/taxonomizer.jl"))
