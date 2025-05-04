1f02247321e9bf929297224974b42dc46e744682
Collin Schwantes <cjschwantesll@gmail.com>
2025-05-03 23:09:19 -0400

    adding targets workflow

---
+++ Code/sys_deps.sh
@@ -0,0 +1,7 @@
+apt-get update && apt-get install -y --no-install-recommends \
+          libcurl4-openssl-dev \
+          libssl-dev \
+          libharfbuzz-dev \
+          libfribidi-dev \
+          libcurl4-openssl-dev \
+          libarchive-dev
