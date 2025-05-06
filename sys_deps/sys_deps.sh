901b808d820b7dbd4f8dc05957c64ad5d6d61dea
collinschwantes <collin@viralemergence.org>
2025-05-06 11:22:28 -0400

    adding dependencies and installing julia with juliaup

---
+++ sys_deps/sys_deps.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get install -y --no-install-recommends \
+          libcurl4-openssl-dev \
+          libssl-dev \
+          libarchive-dev \
+          libharfbuzz-dev \
+          libfribidi-dev
