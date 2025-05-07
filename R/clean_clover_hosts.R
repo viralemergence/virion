f5cc1cd8ae1aee3ff07433732fab5f5130364966
collinschwantes <collin@viralemergence.org>
2025-05-06 22:55:18 -0400

    specify package for replace_na

--- R/clean_clover_hosts.R
+++ R/clean_clover_hosts.R
@@ -1,7 +1,7 @@
 clean_clover_hosts <- function(virion_ictv_ratified){
   
   virion_ictv_ratified %<>% 
-    dplyr::mutate(HostFlagID = replace_na(HostFlagID, FALSE)) 
+    dplyr::mutate(HostFlagID = tidyr::replace_na(HostFlagID, FALSE)) 
   
   virion_ictv_ratified %<>%
     mutate_cond(stringr::str_detect(HostOriginal, " cf\\."), HostFlagID = TRUE) 
