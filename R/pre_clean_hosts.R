f0aedf1c3a75f53fe06b85bc4ec341a6c8b59a0b
collinschwantes <collin@viralemergence.org>
2025-05-30 18:10:23 -0400

    adding documentation

--- R/pre_clean_hosts.R
+++ R/pre_clean_hosts.R
@@ -1,24 +1,23 @@
-#' .. content for \description{} (no empty lines) ..
+#' Align predict hosts with the NCBI backbone
 #'
-#' .. content for \details{} ..
+#' 
 #'
-#' @title
-#' @param predict_all_formatted
-#' @param pre_host_table
+#' @param predict_all_formatted data frame. Predict data
+#' @param pre_host_table data frame. host table from jhdict
 #' @return
 #' @author collinschwantes
 #' @export
 pre_clean_hosts <- function(predict_all_formatted, pre_host_table) {
 
   # drop everything in clo except host
-  clo_col_filter <- !names(pre_host_table) %in% "Host"
-  clo_cols_to_drop <- names(pre_host_table)[clo_col_filter]
+  pre_col_filter <- !names(pre_host_table) %in% "Host"
+  pre_cols_to_drop <- names(pre_host_table)[pre_col_filter]
   
 
   
   # join to NCBI data
   out <- predict_all_formatted %>% 
-    dplyr::select(-all_of(clo_cols_to_drop)) %>% 
+    dplyr::select(-all_of(pre_cols_to_drop)) %>% 
     dplyr::rename(HostOriginal = "Host") %>%
     dplyr::mutate(HostOriginal = stringr::str_to_sentence(HostOriginal)) %>% 
     dplyr::left_join(pre_host_table,by = "HostOriginal") 
