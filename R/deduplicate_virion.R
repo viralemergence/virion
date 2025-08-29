60907a71f0aaba4e66b636d2e04e35cd4f85aab0
collinschwantes <collin@viralemergence.org>
2025-08-29 13:21:04 -0400

    reducing overhead in the dedupe function

--- R/deduplicate_virion.R
+++ R/deduplicate_virion.R
@@ -14,24 +14,24 @@
 deduplicate_virion <- function(virion_clover_hosts, path = "outputs/virion.csv.gz"){
   
   print("deduplicate_virion: starting")
-  v_distinct <- virion_clover_hosts %>% 
+  v_ <- virion_clover_hosts %>% 
     dplyr::distinct()
   print("deduplicate_virion: distinct")
-  v_char <- v_distinct %>% 
+  v_ <- v_ %>% 
     dplyr::mutate_all(as.character) 
   print("deduplicate_virion: character")
   
-  v_blank <- v_char %>%   
+  v_ <- v_ %>%   
     dplyr::mutate_all(~tidyr::replace_na(.x, ''))
   print("deduplicate_virion: blank")
   
-out <- v_blank %>% 
+  v_ <- v_ %>% 
   dplyr::group_by_at(dplyr::vars(-NCBIAccession)) %>% 
     dplyr::summarize(NCBIAccession = stringr::str_c(NCBIAccession, collapse = ", ")) %>% 
     dplyr::ungroup()
   print("deduplicate_virion: deduplicated")
   
-  out_path <- vroom::vroom_write(out, path,delim = ",")
+  out_path <- vroom::vroom_write(v_, path,delim = ",")
   print("deduplicate_virion: file written")
     
   return(out_path)
