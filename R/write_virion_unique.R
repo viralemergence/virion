6d3b1bb0e67167a65d03676a9945c3f74281db2d
collinschwantes <collin@viralemergence.org>
2026-04-10 19:39:16 -0400

    writes the clean and unique version of virion

---
+++ R/write_virion_unique.R
@@ -0,0 +1,23 @@
+#' Shrinks then writes the data
+#' 
+#' Removes NCBIAccession numbers as a field then keeps distinct values.
+#' Writes this much smaller dataset with vroom.
+#' 
+#' 
+#' @param viron_unique Dataframe. Cleaned and unique records from make_virion_unique
+#' @param file String. Location for vroom to write to. 
+#' @return Dataframe. Data written via vroom.
+#' @author collinschwantes
+
+write_virion_unique <- function(viron_unique = viron_unique, file =
+                                "outputs/virion.csv.gz") {
+
+  virion_out <- viron_unique |>
+    dplyr::select(-NCBIAccession) |>
+    dplyr::distinct()
+
+  vroom::vroom_write(virion_out,file = "outputs/virion.csv.gz",
+                     delim = ","
+                     )
+  
+}
