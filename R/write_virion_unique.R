388689e9574bbb0801ebbf78161d7f991e0f5dc2
collinschwantes <collin@viralemergence.org>
2026-04-20 11:49:53 -0400

    adding messages and usage updates

--- R/write_virion_unique.R
+++ R/write_virion_unique.R
@@ -11,11 +11,20 @@
 
 write_virion_unique <- function(virion_unique, file =
                                 "outputs/virion.csv.gz") {
-
+  
+  system("sh ./sys_deps/sys_specs.sh")
+  
+  print("dropping NCBI accession numbers and keeping distinct records")
+  
   virion_out <- virion_unique |>
     dplyr::select(-NCBIAccession) |>
     dplyr::distinct()
+  
+  system("sh ./sys_deps/sys_specs.sh")
+  
+  print("dropped and distinct")
 
+  print("writing csv.gz")
   vroom::vroom_write(virion_out,file = file,
                      delim = ","
                      )
